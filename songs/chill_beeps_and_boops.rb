0df7145def1fef56916f9c74cc96b490f7cd3ab0
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2020-04-28 23:15:34 +0900

    Move fx to be able to control

--- songs/chill_beeps_and_boops.rb
+++ songs/chill_beeps_and_boops.rb
@@ -16,7 +16,7 @@ live_loop :blips, sync: :bar do
   with_fx :reverb, mix: 0.6, room: 1 do |rev|
     remaining = 4
     while remaining > 0
-      play (scale :c, :major).choose, amp: rand, pan: rrand(-1, 1)
+      #play (scale :c, :major).choose, amp: rand, pan: rrand(-1, 1)
       s = [0.25, 0.5, 0.75].choose
       if remaining < s then
         s = remaining
@@ -30,10 +30,10 @@ end
 live_loop :drums, sync: :bar do
   with_fx :reverb do |rev|
     control rev, room: 0.6, damp: 0.5
-    sample :drum_bass_hard, amp: 1
+    #sample :drum_bass_hard, amp: 1
     sleep 2
     control rev, room: 1, damp: 0.1
-    sample :sn_zome
+    #sample :sn_zome
     sleep 2
   end
 end
@@ -47,12 +47,13 @@ live_loop :hh, sync: :bar do
   end
 end
 
-with_fx :slicer do |sl|
-  with_fx :lpf, cutoff_slide: 1 do |hp|
-    live_loop :bass, sync: :bar do
+live_loop :bass, sync: :bar do
+  with_fx :slicer do |sl|
+    with_fx :lpf, cutoff_slide: 2 do |hp|
       use_synth :chipbass
-      control hp, cutoff: (ring 70, 130).tick
-      play [:c2, :e2, :a2].choose, release: 2.5
+      control hp, cutoff: (ring 60, 130).tick
+      control sl, phase: 0.25, wave: 1
+      #play [:c2, :e2, :a2].choose, release: 2.5
       sleep 2
     end
   end
